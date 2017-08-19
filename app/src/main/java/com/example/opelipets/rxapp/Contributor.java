795314eb8dd3c49884aee762afdee6a56892ce8a
Dmitry Sumtsov <yc14ds3@macys.com>
2017-08-19 22:04:51 +0300

    alignment

--- app/src/main/java/com/example/opelipets/rxapp/Contributor.java
+++ app/src/main/java/com/example/opelipets/rxapp/Contributor.java
@@ -3,12 +3,11 @@ package com.example.opelipets.rxapp;
 public class Contributor {
 
     String login;
-    String html_url;
 
     int contributions;
 
     @Override
     public String toString() {
-        return login + " (" + contributions + ")";
+        return login + " (" + contributions + ")\n";
     }
 }
